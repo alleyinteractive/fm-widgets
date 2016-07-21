bc17295c55d1641a74b9603c78e6e9e760aa2223
Matt Boynes <mboynes@alleyinteractive.com>
2016-07-20 22:08:14 -0400

    Send the right context in ajax after widget reload

--- fm-widgets.php
+++ fm-widgets.php
@@ -16,3 +16,11 @@ define( 'FM_WIDGETS_URL', trailingslashit( plugins_url( '', __FILE__ ) ) );
 if ( class_exists( 'Fieldmanager_Field' ) && ! class_exists( 'FM_Widget' ) ) {
 	require_once( __DIR__ . '/inc/class-fm-widget.php' );
 }
+
+function fm_widgets_calculated_context( $context ) {
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX && ! empty( $_POST['action'] ) && 'save-widget' === $_POST['action'] ) {
+		return [ 'widget', null ];
+	}
+	return $context;
+}
+add_filter( 'fm_calculated_context', 'fm_widgets_calculated_context' );
