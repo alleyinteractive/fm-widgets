f51265b78bcce31825ddf2fd8718015d8bbdea51
Matt Boynes <mboynes@alleyinteractive.com>
2016-07-24 23:53:30 -0400

    Use fm_activate_tab instead of fm_collapsible_toggle
    Resolves #6

--- static/fm-widget.js
+++ static/fm-widget.js
@@ -8,13 +8,13 @@ jQuery( function( $ ) {
 		if ( target.parents( '.widget-top' ).length && ! target.parents( '#available-widgets' ).length ) {
 			widget = target.closest( 'div.widget' );
 			if ( widget.hasClass( 'open' ) ) {
-				$( document ).trigger( 'fm_collapsible_toggle' );
+				$( document ).trigger( 'fm_activate_tab' );
 			}
 		}
 	});
 
 	$( document ).bind( 'widget-added widget-updated', function() {
-		$( document ).trigger( 'fm_collapsible_toggle' );
+		$( document ).trigger( 'fm_activate_tab' );
 		if ( fm && fm.init_display_if ) {
 			$( '.display-if' ).each( fm.init_display_if );
 		}
